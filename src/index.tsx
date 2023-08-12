eb0c5ef29a705d1947bab891fd251eb1e22dd429
Kirill Ramzaev <k.ramzaev@auto-sys.su>
2023-08-12 16:02:34 +0300

    added valueProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
+import { InputValueProvider } from './context/InputValueProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <InputValueProvider>
+    <App />
+  </InputValueProvider>,
+  document.getElementById('root')
+);
