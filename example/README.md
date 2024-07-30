190b093c303c52190beccf55ca10792c73153c70
Kirill Ramzaev <k.ramzaev@auto-sys.su>
2024-07-30 15:32:39 +0300

    update example

---
+++ example/README.md
@@ -0,0 +1,9 @@
+# Basic Example
+
+This example demonstrates the basic usage
+
+## Preview
+
+Open this example in [StackBlitz](https://stackblitz.com):
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/fork/github/Ramziij/react-tailwind-tooltip/tree/main/example?title=react-tailwind-tooltip-example&file=src/App.tsx)
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import { useMuiKeyboard } from '../../src/context';
+import { useMuiKeyboard } from '@autosys/react-base-keyboard';
 
 const App = () => {
   const { inputValue, keyBoard, keyboardFeature } = useMuiKeyboard();
   return (
-    <div>
-      <input
-        type="text"
-        className="input input-bordered input-primary w-full max-w-xs"
-        value={inputValue}
-      />
-      {keyBoard}
-      <button className="btn" onClick={() => keyboardFeature({ openKeyboard: true })}>
-        {'open'}
-      </button>
+    <div className="flex flex-col justify-center items-start ml-10 mt-10">
+      <div className="flex flex-row">
+        <input
+          type="text"
+          className="input input-bordered input-primary w-full max-w-xs"
+          value={inputValue}
+          onClick={() => keyboardFeature({ openKeyboard: true })}
+        />
+
+        <button className="btn ml-3" onClick={() => keyboardFeature({ openKeyboard: false })}>
+          {'Close'}
+        </button>
+      </div>
+      <div>{keyBoard}</div>
     </div>
   );
 };
